a77488288bec08f72a4b824aefd31260c7204026
b-bikash <bikashbaruah20@gmail.com>
2025-12-15 14:30:24 +0530

    Add EF Core DbContext and initial migration

---
+++ OnlineExamSystem/OnlineExamSystem/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace OnlineExamSystem.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
