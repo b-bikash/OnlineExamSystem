bcf9516d39022d0529f9d51dbf28a5739672fb47
b-bikash <bikashbaruah20@gmail.com>
2025-12-26 17:03:34 +0530

    Add EF Core migrations and database models

---
+++ OnlineExamSystem/OnlineExamSystem/Migrations/20251226113108_AddExamBehaviorLog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineExamSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExamBehaviorLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExamBehaviorLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    ExamId = table.Column<int>(type: "int", nullable: false),
+                    TabSwitchCount = table.Column<int>(type: "int", nullable: false),
+                    FullscreenExitCount = table.Column<int>(type: "int", nullable: false),
+                    AvgTimePerQuestion = table.Column<double>(type: "float", nullable: true),
+                    TotalExamTime = table.Column<double>(type: "float", nullable: true),
+                    ViolationCount = table.Column<int>(type: "int", nullable: false),
+                    IsSuspicious = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExamBehaviorLogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExamBehaviorLogs");
+        }
+    }
+}
