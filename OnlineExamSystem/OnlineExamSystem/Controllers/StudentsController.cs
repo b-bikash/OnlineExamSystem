f14f19588fe1a9a63a12b0a26484538ed3d3dd04
b-bikash <bikashbaruah20@gmail.com>
2026-01-09 16:33:00 +0530

    Added Students CRUD (Create & List)

---
+++ OnlineExamSystem/OnlineExamSystem/Controllers/StudentsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineExamSystem.Models;
+using System.Linq;
+
+namespace OnlineExamSystem.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var students = _context.Students.ToList();
+            return View(students);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+USE OnlineExamDB;
+
+INSERT INTO Students (Name)
+VALUES ('Bikash Baruah');
+
+SELECT * FROM Students;
