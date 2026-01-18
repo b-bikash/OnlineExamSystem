40eab774c74a69208c08175e61f3b739da4d337c
b-bikash <bikashbaruah20@gmail.com>
2026-01-18 21:39:43 +0530

    Update Exam model and add exam fields migration

--- OnlineExamSystem/OnlineExamSystem/Models/Exam.cs
+++ OnlineExamSystem/OnlineExamSystem/Models/Exam.cs
@@ -1,9 +1,25 @@
-﻿namespace OnlineExamSystem.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineExamSystem.Models
 {
     public class Exam
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
         public string Title { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [Required]
         public int DurationInMinutes { get; set; }
+
+        [Required]
+        public int TotalMarks { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
