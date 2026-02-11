291d22ddf28b9ff2038e71d41c641a756ecf812f
b-bikash <bikashbaruah20@gmail.com>
2026-02-11 16:56:28 +0530

    Added migration for model updates

---
+++ OnlineExamSystem/OnlineExamSystem/Migrations/20260211112539_MakeCollegeIdNullable.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineExamSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeCollegeIdNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Teachers_Colleges_CollegeId",
+                table: "Teachers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CollegeId",
+                table: "Teachers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CollegeId",
+                table: "Exams",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Teachers_Colleges_CollegeId",
+                table: "Teachers",
+                column: "CollegeId",
+                principalTable: "Colleges",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Teachers_Colleges_CollegeId",
+                table: "Teachers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CollegeId",
+                table: "Teachers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CollegeId",
+                table: "Exams",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Teachers_Colleges_CollegeId",
+                table: "Teachers",
+                column: "CollegeId",
+                principalTable: "Colleges",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
