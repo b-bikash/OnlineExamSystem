0e4942d71cd70ffc7c2cb3d2ede11f3c4e775d24
b-bikash <bikashbaruah20@gmail.com>
2026-03-10 16:44:27 +0530

    Added migration for ExamProctorLog

---
+++ OnlineExamSystem/OnlineExamSystem/Migrations/20260310111303_AddExamProctorLog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineExamSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExamProctorLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExamProctorLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ExamAttemptId = table.Column<int>(type: "int", nullable: false),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CapturedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SuspiciousFlag = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExamProctorLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExamProctorLogs_ExamAttempts_ExamAttemptId",
+                        column: x => x.ExamAttemptId,
+                        principalTable: "ExamAttempts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExamProctorLogs_ExamAttemptId",
+                table: "ExamProctorLogs",
+                column: "ExamAttemptId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExamProctorLogs");
+        }
+    }
+}
