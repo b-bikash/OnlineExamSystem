3974ce5d23ed3319aeabebbe2fcac000209542bd
b-bikash <bikashbaruah20@gmail.com>
2026-03-11 11:44:36 +0530

    Added Migration for AddSuspiciousReasonToProctorLog

---
+++ OnlineExamSystem/OnlineExamSystem/Migrations/20260311061342_AddSuspiciousReasonToProctorLog.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineExamSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSuspiciousReasonToProctorLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SuspiciousReason",
+                table: "ExamProctorLogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SuspiciousReason",
+                table: "ExamProctorLogs");
+        }
+    }
+}
