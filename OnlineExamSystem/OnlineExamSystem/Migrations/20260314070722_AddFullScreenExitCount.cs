6446103dbb06f1e63a5175ea3ce695fc67a17684
b-bikash <bikashbaruah20@gmail.com>
2026-03-14 12:38:06 +0530

    Added Migration for AddFullScreenExitCount

---
+++ OnlineExamSystem/OnlineExamSystem/Migrations/20260314070722_AddFullScreenExitCount.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineExamSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFullScreenExitCount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FullScreenExitCount",
+                table: "ExamAttempts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullScreenExitCount",
+                table: "ExamAttempts");
+        }
+    }
+}
